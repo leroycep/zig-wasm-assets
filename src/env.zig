67b63fcb98df43a059ffdbef66199bebe46deb04
LeRoyce Pearson <leroycepearson@geemili.xyz>
2021-02-04 19:14:18 -0700

    Move FFI stuff to `src/env.zig`

---
+++ src/env.zig
@@ -0,0 +1,47 @@
+const std = @import("std");
+
+extern fn do_fetch(filename_ptr: [*]const u8, filename_len: usize, cb: *c_void, data_out: *[]u8) void;
+
+pub fn fetch(file_name: []const u8) ![]const u8 {
+    var data: []u8 = undefined;
+    suspend do_fetch(file_name.ptr, file_name.len, @frame(), &data);
+    return data;
+}
+
+export fn _finalize_fetch(cb_void: *c_void, data_out: *[]u8, buffer: [*]u8, len: usize) void {
+    const cb = @ptrCast(anyframe, @alignCast(8, cb_void));
+    data_out.* = buffer[0..len];
+    resume cb;
+}
+
+extern fn console_log_write(str_ptr: [*]const u8, str_len: usize) void;
+extern fn console_log_flush() void;
+
+fn consoleLogWrite(context: void, bytes: []const u8) error{}!usize {
+    console_log_write(bytes.ptr, bytes.len);
+    return bytes.len;
+}
+
+fn consoleLogWriter() std.io.Writer(void, error{}, consoleLogWrite) {
+    return .{ .context = {} };
+}
+
+pub fn log(
+    comptime message_level: std.log.Level,
+    comptime scope: @Type(.EnumLiteral),
+    comptime format: []const u8,
+    args: anytype,
+) void {
+    const writer = consoleLogWriter();
+    defer console_log_flush();
+    writer.print("{s}: {s}: ", .{ std.meta.tagName(message_level), std.meta.tagName(scope) }) catch {};
+    writer.print(format, args) catch {};
+}
+
+pub fn panic(msg: []const u8, stacktrace: ?*std.builtin.StackTrace) noreturn {
+    console_log_write(msg.ptr, msg.len);
+    console_log_flush();
+    while (true) {
+        @breakpoint();
+    }
+}
--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,9 @@
 const std = @import("std");
 const zee_alloc = @import("zee_alloc");
+const env = @import("./env.zig");
+
+pub const log = env.log;
+pub const panic = env.panic;
 
 const allocator: *std.mem.Allocator = zee_alloc.ZeeAllocDefaults.wasm_allocator;
 comptime {
@@ -20,58 +24,14 @@ pub fn main() anyerror!void {
 pub fn asyncMain() anyerror!void {
     std.log.info("All your codebase are belong to us.", .{});
 
-    var hello_fetch = async fetch("hello.txt");
-    var world_fetch = async fetch("world.txt");
+    var hello_fetch = async env.fetch("hello.txt");
+    var world_fetch = async env.fetch("world.txt");
 
     const hello_contents = try await hello_fetch;
+    defer allocator.free(hello_contents);
     const world_contents = try await world_fetch;
+    defer allocator.free(world_contents);
 
     std.log.info("contents of file: {s}", .{hello_contents});
     std.log.info("contents of world file: {s}", .{world_contents});
-}
-
-extern fn do_fetch(filename_ptr: [*]const u8, filename_len: usize, cb: *c_void, data_out: *[]u8) void;
-
-pub fn fetch(file_name: []const u8) ![]const u8 {
-    var data: []u8 = undefined;
-    suspend do_fetch(file_name.ptr, file_name.len, @frame(), &data);
-    return data;
-}
-
-export fn _finalize_fetch(cb_void: *c_void, data_out: *[]u8, buffer: [*]u8, len: usize) void {
-    const cb = @ptrCast(anyframe, @alignCast(8, cb_void));
-    data_out.* = buffer[0..len];
-    resume cb;
-}
-
-extern fn console_log_write(str_ptr: [*]const u8, str_len: usize) void;
-extern fn console_log_flush() void;
-
-fn consoleLogWrite(context: void, bytes: []const u8) error{}!usize {
-    console_log_write(bytes.ptr, bytes.len);
-    return bytes.len;
-}
-
-fn consoleLogWriter() std.io.Writer(void, error{}, consoleLogWrite) {
-    return .{ .context = {} };
-}
-
-pub fn log(
-    comptime message_level: std.log.Level,
-    comptime scope: @Type(.EnumLiteral),
-    comptime format: []const u8,
-    args: anytype,
-) void {
-    const writer = consoleLogWriter();
-    defer console_log_flush();
-    writer.print("{s}: {s}: ", .{ std.meta.tagName(message_level), std.meta.tagName(scope) }) catch {};
-    writer.print(format, args) catch {};
-}
-
-pub fn panic(msg: []const u8, stacktrace: ?*std.builtin.StackTrace) noreturn {
-    console_log_write(msg.ptr, msg.len);
-    console_log_flush();
-    while (true) {
-        @breakpoint();
-    }
 }
